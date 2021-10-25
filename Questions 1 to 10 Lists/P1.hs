6da46006b5a7efda22b7d2f26aa8b2fceb471004
vmdokdc <vuminhduc.vmd.vn@gmail.com>
2021-10-25 19:20:28 +0700

    Complete P1.hs

--- Questions 1 to 10 Lists/P1.hs
+++ Questions 1 to 10 Lists/P1.hs
@@ -1,1 +1,10 @@
 -- Find the last element of a list
+myLast :: [a] -> a
+myLast [] = error "Empty list"
+myLast [x] = x
+myLast (_:end_ele) = myLast end_ele
+
+{-
+main :: IO()
+main = print $myLast [1, 4, 2, 5, 3, 1]
+-}
