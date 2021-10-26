c6bebc359063e07b6b2fc31b1c08461885ce9e49
vmdokdc <vuminhduc.vmd.vn@gmail.com>
2021-10-26 22:05:50 +0700

    add P1 to P5

--- Questions 1 to 10 Lists/P1.hs
+++ Questions 1 to 10 Lists/P1.hs
@@ -4,6 +4,9 @@ myLast [] = error "Empty list"
 myLast [x] = x
 myLast (_:end_ele) = myLast end_ele
 
+myLast' :: [a] -> a
+myLast' = last
+
 {-
 main :: IO()
 main = print $myLast [1, 4, 2, 5, 3, 1]
